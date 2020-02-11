ebfe34f50fef2321a324cb22451d0779c4b410a7
Tiffany Obi <tiffanyobi123@gmail.com>
2020-02-11 10:18:25 -0500

    created tab bar and their 3 view controllers. set up windowScene and deleted all trace of mainstoryboard  including file and initial view controller.

---
+++ Unit4Assessment/Unit4Assessment/Controllers/FlashCardsTabController.swift
@@ -0,0 +1,21 @@
+//
+//  FlashCardsTabController.swift
+//  Unit4Assessment
+//
+//  Created by Tiffany Obi on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class FlashCardsTabController: UITabBarController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Unit4Assessment/Unit4Assessment/Controllers/SearchFlashcardsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchFlashcardsViewController.swift
+//  Unit4Assessment
+//
+//  Created by Tiffany Obi on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class SearchFlashcardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
