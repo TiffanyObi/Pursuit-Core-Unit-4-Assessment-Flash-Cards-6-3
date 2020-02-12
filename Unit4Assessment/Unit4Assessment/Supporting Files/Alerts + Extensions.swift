8e246eb118f77b15352a436460fd7fce023674dc
Tiffany Obi <tiffanyobi123@gmail.com>
2020-02-11 19:10:27 -0500

    added alert controller

---
+++ Unit4Assessment/Unit4Assessment/Supporting Files/Alerts + Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Alerts + Extensions.swift
+//  Unit4Assessment
+//
+//  Created by Tiffany Obi on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+  func showAlert(title: String, message: String, completion: ((UIAlertAction) -> Void)? = nil) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: completion)
+    alertController.addAction(okAction)
+    present(alertController, animated: true, completion: nil)
+  }
+}
